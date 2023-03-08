4bd4c05a90e0d9e1ecee0a45a9c1ee7ebd28bfc3
Vinu-Balan <vinubalan2892002@gmail.com>
2023-03-08 18:47:00 +0530

    cors

--- index.js
+++ index.js
@@ -17,7 +17,12 @@ var messages = require("./model/messages.js");
 const app = express();
 const  PORT = 3306;
 
-app.use(cors({origin: true, credentials: true}));
+// app.use(cors({origin: true, credentials: true}));
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 app.use(express.json())
 // Route to get all posts
 
